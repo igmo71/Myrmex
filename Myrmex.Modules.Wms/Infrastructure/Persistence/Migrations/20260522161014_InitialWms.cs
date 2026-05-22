b48f538957e668cc2cc19371f8a6b7d8193b5576
Igor Mogilnitskiy <igmo71@outlook.com>
2026-05-22 19:13:01 +0300

    #12 Add-Migration InitialWms

---
+++ Myrmex.Modules.Wms/Infrastructure/Persistence/Migrations/20260522161014_InitialWms.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Myrmex.Modules.Wms.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialWms : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "wms");
+
+            migrationBuilder.CreateTable(
+                name: "warehouses",
+                schema: "wms",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Code = table.Column<string>(type: "nvarchar(32)", maxLength: 32, nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: true),
+                    CreatedAtUtc = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    UpdatedAtUtc = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_wms_warehouses", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "UX_wms_warehouses_code",
+                schema: "wms",
+                table: "warehouses",
+                column: "Code",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "warehouses",
+                schema: "wms");
+        }
+    }
+}
