07bc4b217ca7aa50b441f5b39f6605bab5b77c94
Igor Mogilnitskiy <igmo71@outlook.com>
2026-05-23 19:57:38 +0300

    #18 StorageLocation entities

---
+++ Myrmex.Modules.Wms/Topology/Domain/StorageLocations/StorageLocation.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Myrmex.Modules.Wms.Topology.Domain.StorageLocations;
+
+internal class StorageLocation
+{
+}
---
+++ Myrmex.Modules.Wms/Topology/Domain/StorageLocations/StorageLocationEvents.cs
@@ -0,0 +1,29 @@
+﻿using Myrmex.Core.Events;
+
+namespace Myrmex.Modules.Wms.Topology.Domain.StorageLocations;
+
+internal sealed record StorageLocationCreatedDomainEvent(
+    Guid StorageLocationId,
+    Guid WarehouseId,
+    Guid ZoneId) : IDomainEvent;
+
+internal sealed record StorageLocationDetailsUpdatedDomainEvent(
+    Guid StorageLocationId,
+    Guid WarehouseId,
+    Guid ZoneId) : IDomainEvent;
+
+internal sealed record StorageLocationStatusChangedDomainEvent(
+    Guid StorageLocationId,
+    Guid WarehouseId,
+    Guid ZoneId,
+    Guid StorageLocationStatusId) : IDomainEvent;
+
+internal sealed record StorageLocationDeactivatedDomainEvent(
+    Guid StorageLocationId,
+    Guid WarehouseId,
+    Guid ZoneId) : IDomainEvent;
+
+internal sealed record StorageLocationReactivatedDomainEvent(
+    Guid StorageLocationId,
+    Guid WarehouseId,
+    Guid ZoneId) : IDomainEvent;
