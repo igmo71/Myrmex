be4b6a015d73b8339ffb221fc4b90178070e0bd5
Igor Mogilnitskiy <igmo71@outlook.com>
2026-06-16 16:23:28 +0300

    #58 Refactor UI for InventoryBalance

---
+++ Myrmex.WebApp/Wms/Inventory/InventoryBalanceDetails.cs
@@ -0,0 +1,32 @@
+﻿namespace Myrmex.WebApp.Wms.Inventory;
+
+public sealed record InventoryBalanceDetails(
+    Guid Id,
+    decimal Quantity,
+    DateTimeOffset CreatedAtUtc,
+    DateTimeOffset? UpdatedAtUtc,
+    InventoryBalanceDetails.SkuInfo Sku,
+    InventoryBalanceDetails.LocationInfo Location)
+{
+    public sealed record SkuInfo(
+        Guid Id,
+        string Code,
+        string Name,
+        UomInfo BaseUom);
+
+    public sealed record UomInfo(
+        Guid Id,
+        string Code,
+        string? Symbol);
+
+    public sealed record LocationInfo(
+        Guid Id,
+        string Code,
+        string Name,
+        WarehouseInfo Warehouse);
+
+    public sealed record WarehouseInfo(
+        Guid Id,
+        string Code,
+        string Name);
+}
