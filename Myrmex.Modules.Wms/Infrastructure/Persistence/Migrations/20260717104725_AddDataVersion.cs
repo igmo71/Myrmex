232f586a7d230db983379517d2743f09abeea756
Igor Mogilnitskiy <igmo71@outlook.com>
2026-07-17 13:53:43 +0300

    #109 Add Migration

---
+++ Myrmex.Modules.Wms/Infrastructure/Persistence/Migrations/20260717104725_AddDataVersion.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Myrmex.Modules.Wms.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDataVersion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "ExternalDataVersion",
+                schema: "wms",
+                table: "warehouses",
+                type: "varbinary(128)",
+                maxLength: 128,
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "ExternalDataVersion",
+                schema: "wms",
+                table: "units_of_measure",
+                type: "varbinary(128)",
+                maxLength: 128,
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "ExternalDataVersion",
+                schema: "wms",
+                table: "stock_keeping_units",
+                type: "varbinary(128)",
+                maxLength: 128,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExternalDataVersion",
+                schema: "wms",
+                table: "warehouses");
+
+            migrationBuilder.DropColumn(
+                name: "ExternalDataVersion",
+                schema: "wms",
+                table: "units_of_measure");
+
+            migrationBuilder.DropColumn(
+                name: "ExternalDataVersion",
+                schema: "wms",
+                table: "stock_keeping_units");
+        }
+    }
+}
